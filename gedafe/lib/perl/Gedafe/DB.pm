4379f2b8132c856064bd8b7e89e1fbd36fb29400
David Schweikert <david@schweikert.ch>
2002-09-25 12:03:39 +0000

    INSERT with 7.2.2 says 0 rows affected... also do not treat as error
    git-svn-id: svn://svn.ee.ethz.ch/gedafe/branches/br1_0@207 c5e4ef7b-cb2b-0410-bad8-cbb44b149fe0

--- gedafe/lib/perl/Gedafe/DB.pm
+++ gedafe/lib/perl/Gedafe/DB.pm
@@ -830,7 +830,7 @@ sub DB_AddRecord($$$$)
 		$$err=$dbh->errstr;
 		return undef;
 	}
-	if($res != 1) {
+	if($res ne 1 and $res ne '0E0') {
 		$$err = "Number of rows affected is not 1! ($res)";
 		return undef;
 	}
